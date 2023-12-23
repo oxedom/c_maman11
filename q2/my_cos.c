000cbbe2d1e754787f98f863665e6cb1ba68b0bd
Sam <oxedom@gmail.com>
2023-12-23 19:03:06 +0200

    fix(q2): Everything works - #main

--- q2/my_cos.c
+++ q2/my_cos.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
+#include <math.h>
 /* The value of pie */
 #define PIE 3.14159265358979
 
 /* Function prototypes */
-int factorial(int n);
+
+double factorial(int n);
 double my_cos(double angle);
 
 int main()
 {
+    /*printf("Enter the angle in degrees: \n"); */
+    /* Initialize variables */
 
+    double my_value = my_cos(80);
+    printf("%f\n", my_value);
     return 0;
 }
 /* Function to calculate cos */
-double my_cos(double angle)
+double my_cos(double degree)
 {
+    int i = 0;
+    double radian = degree * PIE / 180.0;
+    double value = cos(radian);
+    printf("%f\n", value);
+    double result = 0.0;
+    double term = 1.0;
+    while (fabs(term) >= 1e-6)
+    {
+        term = pow(-1, i) * pow(radian, 2 * i) / factorial(2 * i);
+        result += term;
+        i++;
+    }
 
-    return 0;
+    return result;
 }
 
 /* Recursive function to calculate factorial */
-int factorial(int n)
+double factorial(int n)
 {
     if (n == 0 || n == 1)
     {
